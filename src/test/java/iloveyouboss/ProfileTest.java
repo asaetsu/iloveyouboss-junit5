0ce4a45df752328d29fc20057e9f5f7ed3da254e
asano <asaetsu0130@gmail.com>
2016-12-18 15:21:49 +0900

    Chapter 2.4

--- src/test/java/iloveyouboss/ProfileTest.java
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class ProfileTest {
 
+    // 必須の条件にマッチしない場合、matchesはfalseを返す
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet(){
         Profile profile = new Profile("Bull Hockey, Inc.");
@@ -23,5 +24,22 @@ class ProfileTest {
 
         boolean matches = profile.matches(criteria);
         assertFalse(matches);
+    }
+
+    // 不問の条件があればmatchesはtrueを返す
+    @Test
+    public void matchAnswersTrueForAnyDontCareCriteria(){
+        Profile profile = new Profile("Bull Hockey, Inc.");
+        Question question = new BooleanQuestion(1, "牛乳は支給されますか？");
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+        assertTrue(matches);
     }
 }
