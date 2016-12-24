ea12939e2e81b2fedbade170ad286f3153713061
asano <asaetsu0130@gmail.com>
2016-12-24 16:31:05 +0900

    Chapter 3.1.1

---
+++ src/test/java/scratch/AssertTest.java
@@ -0,0 +1,86 @@
+package scratch;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AssertTest {
+
+    class InsufficientFundsException extends RuntimeException {
+        public InsufficientFundsException(String message) {
+            super(message);
+        }
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    class Account {
+        int balance;
+        String name;
+
+        Account(String name) {
+            this.name = name;
+        }
+
+        void deposit(int dollars) {
+            balance += dollars;
+        }
+
+        void withdraw(int dollars) {
+            if (balance < dollars) {
+                throw new InsufficientFundsException("balance only " + balance);
+            }
+            balance -= dollars;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getBalance() {
+            return balance;
+        }
+
+        public boolean hasPositiveBalance() {
+            return balance > 0;
+        }
+    }
+
+    class Customer {
+        List<Account> accounts = new ArrayList<>();
+
+        void add(Account account) {
+            accounts.add(account);
+        }
+
+        Iterator<Account> getAccounts() {
+            return accounts.iterator();
+        }
+
+    }
+
+    private Account account;
+
+    @BeforeEach
+    public void createAccount() {
+        account = new Account("an account name");
+    }
+
+    @Test
+    public void hasPositiveBalance() {
+        account.deposit(50);
+
+        assertTrue(account.hasPositiveBalance());
+    }
+
+    @Test
+    public void depositIncreaseBalance() {
+        int initialBalance = account.getBalance();
+        account.deposit(100);
+        assertTrue(account.getBalance() > initialBalance);
+    }
+}
