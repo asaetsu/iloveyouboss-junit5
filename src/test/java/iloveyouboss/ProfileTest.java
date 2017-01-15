f1298ad736b7f837a6041dee7474264164c85a3a
asano <asaetsu0130@gmail.com>
2017-01-15 14:03:13 +0900

    Chapter 6.6

--- src/test/java/iloveyouboss/ProfileTest.java
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -2,10 +2,7 @@ package iloveyouboss;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-
 import java.util.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -165,5 +162,29 @@ class ProfileTest {
         allAnswers.addAll(answers);
 
         assertThat(ids(allAnswers)).isEqualTo(new int[]{1, 2, 3});
+    }
+
+    @Test
+    public void findAnswers() {
+        int dataSize = 5000;
+        for (int i = 0; i < dataSize; i++) {
+            profile.add(new Answer(new BooleanQuestion(i, String.valueOf(i)), Bool.FALSE));
+        }
+
+        profile.add(new Answer(
+                new PercentileQuestion(
+                        dataSize, String.valueOf(dataSize), new String[]{}), 0));
+
+        int numberOfTimes = 1000;
+        long elapsedMs = run(numberOfTimes, () -> profile.find(a -> a.getQuestion().getClass() == PercentileQuestion.class));
+        assertTrue(elapsedMs < 1000);
+    }
+
+    private long run(int times, Runnable func) {
+        long start = System.nanoTime();
+        for (int i = 0; i < times; i++)
+            func.run();
+        long stop = System.nanoTime();
+        return (stop - start) / 1000000;
     }
 }
