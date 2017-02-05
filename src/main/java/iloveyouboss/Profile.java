7832a6ad1047c3ad46c0a585a6e9d22e62e65ef5
asano <asaetsu0130@gmail.com>
2017-02-05 13:08:14 +0900

    Chapter 9

--- src/main/java/iloveyouboss/Profile.java
+++ src/main/java/iloveyouboss/Profile.java
@@ -1,21 +1,15 @@
 /***
  * Excerpted from "Pragmatic Unit Testing in Java with JUnit",
  * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
+ * Copyrights apply to this code. It may not be used to create training material,
  * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
+ * We make no guarantees that this code is fit for any purpose.
  * Visit http://www.pragmaticprogrammer.com/titles/utj2 for more book information.
  ***/
 package iloveyouboss;
 
-import java.util.*;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
 public class Profile {
-    // 雇用者や求職者に対する質問の答えが保持される
-    private Map<String, Answer> answers = new HashMap<>();
-    private int score;
+    private AnswerCollection answers = new AnswerCollection();
     private String name;
 
     public Profile(String name) {
@@ -27,48 +21,16 @@ public class Profile {
     }
 
     public void add(Answer answer) {
-        answers.put(answer.getQuestionText(), answer);
+        answers.add(answer);
     }
 
-    public boolean matches(Criteria criteria) {
-        calculateScore(criteria);
-        if (doesNotMeetAnyMustMatchCriterion(criteria))
-            return false;
-        return anyMatches(criteria);
+    public MatchSet getMatchSet(Criteria criteria) {
+        return new MatchSet(answers, criteria);
     }
+    // ...
 
-    public int score() {
-        return score;
-    }
-
-    public List<Answer> find(Predicate<Answer> pred) {
-        return answers.values().stream()
-                .filter(pred)
-                .collect(Collectors.toList());
-    }
-
-    private Answer answerMatching(Criterion criterion) {
-        return answers.get(criterion.getAnswer().getQuestionText());
-    }
-
-    private boolean anyMatches(Criteria criteria) {
-        boolean anyMatches = false;
-        for (Criterion criterion: criteria)
-            anyMatches |= criterion.matches(answerMatching(criterion)); return anyMatches;
-    }
-
-    private void calculateScore(Criteria criteria) { score = 0;
-        for (Criterion criterion: criteria)
-            if (criterion.matches(answerMatching(criterion)))
-                score += criterion.getWeight().getValue();
-    }
-
-    private boolean doesNotMeetAnyMustMatchCriterion(Criteria criteria) {
-        for (Criterion criterion : criteria) {
-            boolean match = criterion.matches(answerMatching(criterion));
-            if (!match && criterion.getWeight() == Weight.MustMatch)
-                return true;
-        }
-        return false;
+    @Override
+    public String toString() {
+        return name;
     }
 }
